7769aa155c8ca18a3d170763669ea238c26b3292
gopi gk <gugulothuachamma@gmail.com>
2026-08-01 19:17:29 +0530

    Update generate_multi_playlists.py

--- generate_multi_playlists.py
+++ generate_multi_playlists.py
@@ -17,6 +17,10 @@ PLAYLISTS = [
         "url": "https://thanks-to-veer.saqlainhaider8198.workers.dev/jstar.m3u[jtvf]?ua=sktechtv",
         "category": "Jio Star"
     },
+       {
+        "url": "https://jhsevetns-fhd.rtxcric.workers.dev/playlist.m3u",
+        "category": "Hotstar Events"
+    },
    {
         "url": "https://nonyliv.saqlainhaider8198.workers.dev/nony2.m3u",
         "category": "SonyLiv"
