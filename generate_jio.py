8ffe99dc9b70c1276eee3716c31e0af1678d684e
gopi gk <gugulothuachamma@gmail.com>
2026-08-03 23:34:34 +0530

    Update FRESH_JIO_URL to new GitHub link

--- generate_jio.py
+++ generate_jio.py
@@ -10,7 +10,7 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding
 
 # ─── CONFIG ────────────────────────────────────────────────────────────
-FRESH_JIO_URL = "https://thanks-to-veer.saqlainhaider8198.workers.dev/jtv90.m3u[srisk]?ua=sktechtv"
+FRESH_JIO_URL = "https://raw.githubusercontent.com/SSK4570live/TV-/refs/heads/main/jtv.m3u"
 OUTPUT_FILE   = "gio.m3u"
 RETRY_COUNT   = 5
 RETRY_DELAY   = 10
